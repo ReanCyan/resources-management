b531a4bcf25167172a8fdf073ee9fb687064c1da
Brijesh Joshi <joshi.brijesh002@gmail.com>
2022-06-20 01:02:46 +0530

    Migration & Models added

---
+++ database/migrations/2022_06_19_181139_create_table_pdfs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pdfs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('unique_name');
+            $table->string('original_name');
+            $table->string('extension');
+            $table->string('location');
+            $table->double('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pdfs');
+    }
+};
