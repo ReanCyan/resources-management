72362c7156ae0855bf287566149debb69fbbe240
Brijesh Joshi <joshi.brijesh002@gmail.com>
2022-06-22 13:27:46 +0530

    Added HTML Snippet View functionality

--- resources/js/visitor/components/html_snippet/HtmlSnippetList.vue
+++ resources/js/visitor/components/html_snippet/HtmlSnippetList.vue
@@ -1,16 +1,23 @@
 <template>
   <el-table :data="list" style="width: 100%">
-    <el-table-column prop="title" label="Title" />
     <el-table-column prop="description" label="Description" />
     <el-table-column label="Actions">
       <template #default="scope">
+        <el-link
+          type="primary"
+          href=""
+          :icon="viewIcon"
+          @click="viewSnippet(scope.$index, scope.row)"
+        >
+          View
+        </el-link>
         <el-link
           type="warning"
           href=""
           :icon="CopyDocument"
           @click="copySnippet(scope.$index, scope.row)"
         >
-          Copy HTML Snippet
+          Copy
         </el-link>
       </template>
     </el-table-column>
@@ -19,12 +26,13 @@
 
 <script>
 import { ref } from 'vue';
-import { ElMessage } from 'element-plus';
+import { ElMessage, ElMessageBox } from 'element-plus';
 
 export default {
   name: 'HtmlSnippetList',
   data () {
     return {
+      viewIcon: ref('View'),
       CopyDocument: ref('CopyDocument')
     }
   },
@@ -38,6 +46,17 @@ export default {
     copySnippet (index, row) {
       navigator.clipboard.writeText(row.html);
       ElMessage.success('Snippet Copied to Clipboard');
+    },
+    viewSnippet (index, row) {
+      const h = this.$createElement;
+      ElMessageBox.alert(
+        row.html,
+        'HTML Snippet',
+        {
+          dangerouslyUseHTMLString: true,
+          callback: (action) =>  console.log(`action: ${action}`)
+        }
+      );
     }
   }
 }
